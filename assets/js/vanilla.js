79115deddb00f8737c8f831ed443c61e9a05f21e
Joaquin Fox <Joaquin.Fox@gmail.com>
2020-08-10 12:16:36 -0700

    complete vanilla implementation

--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -1,66 +1,51 @@
-// Select li items and format
+// Variable declarations
 let lis = document.querySelectorAll('li');
 let plus = document.querySelector('.fa-plus');
-let trash = document.querySelectorAll('.fa-trash');
+// let trash = document.querySelectorAll('.fa-trash');
+let trash = document.querySelectorAll('span');
 let input = document.querySelector('#vanilla-input');
 let ul = document.querySelector('ul');
 
-// function applyCompleted() {
+// Toggle class completed
 lis.forEach((li) => {
-  li.addEventListener('click', () => {
+  li.addEventListener('click', (e) => {
+    e.stopPropagation();
     li.classList.toggle('completed');
   });
 });
-// }
-// applyCompleted();
 
-// on click, toggle input reveal
-plus.addEventListener('click', function () {
+// On click, toggle input reveal
+plus.addEventListener('click', function (e) {
+  e.stopPropagation();
   input.classList.toggle('hideinput');
 });
 
-// remove trash items
+// Remove items functionality
 trash.forEach((item) => {
   item.addEventListener('click', () => {
-    item.parentNode.parentNode.remove();
+    item.parentNode.remove();
   });
 });
 
-// Add items to list
-// input.addEventListener('keypress', (e) => {
-//   if (e.keyCode === 13) {
-//     let newItem = e.target.value;
-//     input.value = '';
-//     let liNew = document.createElement('li');
-//     liNew.innerHTML =
-//       '<li><span><i class="fa fa-trash"></i> </span>' + newItem + ' </li>';
-//     ul.appendChild(liNew);
-//     console.log(newItem);
-//   }
-// });
-
+// Create new item and append to list
 input.addEventListener('keypress', (e) => {
   if (e.keyCode === 13) {
     if (input.value) {
       let newItem = e.target.value;
-      input.value = '';
+      input.value = ''; // Clear input
       let liNew = document.createElement('li');
-      // liNew.className = 'completed';
       liNew.innerHTML = '<span><i class="fa fa-trash"></i> </span>' + newItem;
       ul.appendChild(liNew);
-      liNew.addEventListener('click', () => {
+      // Add style toggle to new item
+      liNew.addEventListener('click', (event) => {
+        event.stopPropagation();
         liNew.classList.toggle('completed');
       });
-      // document.querySelector()
-      // let trash = liNew.contains('span');
-      // trash[0].addEventListener('click', () => {
-      //   console.log('hanga banga');
-      // });
-      console.log('TRASH', trash);
-
-      // newItem.classList.add('completed');
-      console.log(newItem);
+      // Add trash functionality to new item
+      let target = liNew.querySelector('.fa-trash');
+      target.addEventListener('click', () => {
+        target.parentNode.parentNode.remove();
+      });
     }
   }
 });
-// applyCompleted();
